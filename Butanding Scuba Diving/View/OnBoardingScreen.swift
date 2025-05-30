563effad452edb3ef874a79157e51c52a6ffa355
Lenard <lenardcortuna05@gmail.com>
2025-05-31 01:04:12 +0800

    On Boarding Screen commit changes

--- Butanding Scuba Diving/View/OnBoardingScreen.swift
+++ Butanding Scuba Diving/View/OnBoardingScreen.swift
@@ -5,3 +5,14 @@
 //  Created by Lenard Cortuna on 5/29/25.
 //
 
+import UIKit
+import SnapKit
+
+class OnBoardingScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+    }
+}
