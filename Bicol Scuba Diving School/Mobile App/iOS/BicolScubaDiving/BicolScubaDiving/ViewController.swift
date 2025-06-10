43e1ad65e56fecf934faeb62eda5141f616a398e
Melvin Ballesteros <>
2025-06-10 17:29:00 +0800

    create initial directory and project

---
+++ Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BicolScubaDiving
+//
+//  Created by Melvin Ballesteros on 6/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDivingTests/BicolScubaDivingTests.swift
@@ -0,0 +1,17 @@
+//
+//  BicolScubaDivingTests.swift
+//  BicolScubaDivingTests
+//
+//  Created by Melvin Ballesteros on 6/10/25.
+//
+
+import Testing
+@testable import BicolScubaDiving
+
+struct BicolScubaDivingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
