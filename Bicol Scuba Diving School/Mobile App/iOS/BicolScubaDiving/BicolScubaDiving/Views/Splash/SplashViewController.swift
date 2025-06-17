0ba536f86451c79c1dc835980a37093b25b088bd
Lenard <lenardcortuna05@gmail.com>
2025-06-17 19:15:10 +0800

    Added Splash Screen feature and Display Version Number

--- Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/Views/Splash/SplashViewController.swift
+++ Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/Views/Splash/SplashViewController.swift
@@ -9,5 +9,50 @@ import UIKit
 
 class SplashViewController: UIViewController {
     
+    var completionHandler: (() -> Void)?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        let logo = UIImageView(image: UIImage(named: "butanding-logo"))
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logo)
+        
+        NSLayoutConstraint.activate([
+            logo.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logo.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            logo.widthAnchor.constraint(equalToConstant: 300),
+            logo.heightAnchor.constraint(equalToConstant: 300)
+        ])
+        
+        // Showing app version
+        let versionLabel = UILabel()
+        versionLabel.translatesAutoresizingMaskIntoConstraints = false
+        versionLabel.textAlignment = .center
+        versionLabel.textColor = .gray
+        versionLabel.font = .systemFont(ofSize: 14, weight: .light)
+        
+        
+        if let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+            versionLabel.text = "Version \(appVersion)"
+        } else {
+            versionLabel.text = "Unknown Version" // Fallback if version can't be retrieved
+        }
+        
+        view.addSubview(versionLabel)
+        
+        NSLayoutConstraint.activate([
+            versionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            versionLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+        ])
+        
+        
+        // Delay transition to Next Screen
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.completionHandler?()
+            
+        }
+    }
     
 }
