8800bfbd08b4b2aa6ba45423607adc0e3cd32a96
Lenard <lenardcortuna05@gmail.com>
2025-06-19 13:39:20 +0800

    Added Changes in SceneDelegate to configure the navigation after Splash Screen.

--- Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/SceneDelegate.swift
+++ Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/SceneDelegate.swift
@@ -26,6 +26,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         self.window = window
         window.makeKeyAndVisible()
         
+        // After Splash is done will transition to OnBoarding
+        splashVc.completionHandler = { [weak self] in
+            DispatchQueue.main.async {
+                let onBoardingVc = OnboardingViewController()
+                self?.window?.rootViewController = onBoardingVc
+                UIView.transition(with: window, duration: 0.5, options: .transitionCrossDissolve, animations: nil)
+            }
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
