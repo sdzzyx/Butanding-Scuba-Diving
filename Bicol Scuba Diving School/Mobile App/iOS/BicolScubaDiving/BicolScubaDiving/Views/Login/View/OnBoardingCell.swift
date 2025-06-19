2015e298c97f80ac5a50b7c0d2896fe4e5fe371d
Lenard <lenardcortuna05@gmail.com>
2025-06-19 13:36:28 +0800

    Added OnBoardingCell for visual content layout

---
+++ Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/Views/Login/View/OnBoardingCell.swift
@@ -0,0 +1,35 @@
+//
+//  OnBoardingCell.swift
+//  BicolScubaDiving
+//
+//  Created by Lenard Cortuna on 6/18/25.
+//
+
+import UIKit
+
+class OnboardingImageCell: UICollectionViewCell {
+    static let reuseIdentifier = "OnboardingImageCell"
+
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ])
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
