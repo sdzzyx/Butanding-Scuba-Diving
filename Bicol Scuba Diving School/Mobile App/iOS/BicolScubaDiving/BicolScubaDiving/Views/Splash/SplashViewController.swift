d71295022857355e7c948f638b0c0ff6366b423f
Lenard Cortuna <116490237+sdzzyx@users.noreply.github.com>
2025-07-03 23:28:11 +0800

    UI Splash Screen Feature (#4)
    * Added feature Splash Screen and Display Version Number
    
    * Configured SceneDelegate
    
    * Added Splash Screen feature and Display Version Number
    
    * Configured Dev Info plist
    
    * Configured Info plist
    
    * Added Changes for AppConstant
    
    * Added OnBoardingViewModel
    
    * Added CustomPageControl for indicators
    
    * Added OnBoardingCell for visual content layout
    
    * Added OnBoardingView for UI elements and layout.
    
    * Added OnBoardingViewController for managing the lifecycle, user interactions and binds the viewModel to OnBoardingView
    
    * Added Changes in SceneDelegate to configure the navigation after Splash Screen.
    
    * Moved OnBoardingView to OnBoardingView folder
    
    * Moved OnBoardingViewController to OnBoarding/Controller folder.
    
    * Refactored OnBoardingView, removing NSLayoutConstraint and using Snapkit for auto layout.
    
    * Refactored SplashViewController removing NSLayoutConstraint and using Snapkit
    
    * Refactored OnBoardingCell, changes for translatesAutoresizingMaskIntoConstraints and replacing it with Snapkit for autolayout of the UI.
    
    * Changes for image file named in OnBoardingViewModel.
    
    * Included the .xcodeproj/project.pbxproj.
    
    * Image assets descriptive filename.
    
    * Stage Changes for Dev info plist.
    
    * Changes on pbxproj.
    
    * Changes on Dev info plist
    
    * Added WelcomeModel.
    
    * Removed the extension Color and Welcome struct.
    
    * Configured OnBoardingViewModel.
    
    * Removed the "Let's Get Started" text and replace the button and text color with the color from ExtensionColor.
    
    * Configured OnBoardingViewController, setting the button title using the view model.
    
    * Changes in pbxproj.
    
    * Created SignUpView.
    
    * Created SignUpViewController.
    
    * Created SignUpViewModel.
    
    * Removed data for SignUp Screen in the AppConstant.
    
    * Instantiate the SignUpViewController for running and testing the UI of Sign Up Screen.
    
    * updated pbxproj.
    
    * Updated pbxproj.
    
    * Delete Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/Resources/Images/Assets.xcassets/SignUp/Contents.json
    
    * Delete Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/Resources/Images/Assets.xcassets/SignUp/back-logo.imageset/Contents.json
    
    * Delete Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/View Models/SignUpViewModel.swift
    
    * Delete Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/Views/SignUp/View/SignUpView.swift
    
    * Delete Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/Views/SignUp/ViewController/SignUpViewController.swift
    
    * Delete Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/Constants/AppConstant.swift
    
    Removed AppConstant changes.
    
    * Update SceneDelegate.swift
    
    Updated SceneDelegate
    
    * Delete Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving.xcodeproj/project.pbxproj
    
    Removed pbxproj changes from SignUpScreen

--- Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/Views/Splash/SplashViewController.swift
+++ Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/Views/Splash/SplashViewController.swift
@@ -6,8 +6,45 @@
 //
 
 import UIKit
+import SnapKit
 
 class SplashViewController: UIViewController {
     
+    var completionHandler: (() -> Void)?
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        let logo = UIImageView(image: UIImage(named: "butanding-logo"))
+        view.addSubview(logo)
+        
+        logo.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.height.equalTo(300)
+        }
+        
+        let versionLabel = UILabel()
+        versionLabel.textAlignment = .center
+        versionLabel.textColor = .gray
+        versionLabel.font = .systemFont(ofSize: 14, weight: .light)
+        
+        if let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+            versionLabel.text = "Version \(appVersion)"
+        } else {
+            versionLabel.text = "Unknown Version"
+        }
+        
+        view.addSubview(versionLabel)
+        
+        versionLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        // Delay transition to Next Screen
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.completionHandler?()
+        }
+    }
 }
