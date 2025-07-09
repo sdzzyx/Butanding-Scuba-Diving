b5c8e06d4a40a55ad590db7df1a9efad782a7d4f
Lenard <lenardcortuna05@gmail.com>
2025-07-09 21:37:13 +0800

    Created WelcomeModel

---
+++ Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/Models/WelcomeModel.swift
@@ -0,0 +1,15 @@
+//
+//  WelcomeModel.swift
+//  BicolScubaDiving
+//
+//  Created by Lenard Cortuna on 7/9/25.
+//
+
+import UIKit
+
+struct WelcomeModel{
+    let image: UIImage
+    let title: String
+    let subtitle: String
+    let description: String
+}
