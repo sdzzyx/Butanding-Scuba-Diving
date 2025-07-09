1d56479278f2e63a75fa48567cdd17a105fcaff9
Melvin Ballesteros <>
2025-07-09 20:54:55 +0800

    [App Navigation] - Created TabViewController and it's logic

---
+++ Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/Models/TabItem.swift
@@ -0,0 +1,15 @@
+//
+//  TabItem.swift
+//  BicolScubaDiving
+//
+//  Created by Melvin Ballesteros on 7/9/25.
+//
+
+import UIKit
+
+struct TabItem {
+    let title: String
+    let imageName: String
+    let tag: Int
+    let viewController: UIViewController    
+}
