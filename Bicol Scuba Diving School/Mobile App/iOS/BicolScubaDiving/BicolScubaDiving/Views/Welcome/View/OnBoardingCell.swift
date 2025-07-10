dd8f335753ae0a12b3f256b3bd64a3f5838eb25b
Lenard <lenardcortuna05@gmail.com>
2025-07-10 14:06:04 +0800

    Configured OnBoardingCell added the titleLabel, subtitleLabel and descriptionLabel.

--- Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/Views/Welcome/View/OnBoardingCell.swift
+++ Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/Views/Welcome/View/OnBoardingCell.swift
@@ -18,12 +18,58 @@ class OnboardingImageCell: UICollectionViewCell {
         return iv
     }()
     
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 28)
+        label.textColor = .primaryOrange
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .italicSystemFont(ofSize: 18)
+        label.textColor = .primaryBlueColor
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 16)
+        label.textColor = .black
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
         contentView.addSubview(imageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(subtitleLabel)
+        contentView.addSubview(descriptionLabel)
         
         imageView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+            make.top.leading.trailing.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.5)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(30)
+            make.leading.trailing.equalToSuperview().inset(20)
+        }
+        
+        subtitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(5)
+            make.leading.trailing.equalTo(titleLabel)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(subtitleLabel.snp.bottom).offset(20)
+            make.leading.trailing.equalTo(titleLabel)
         }
     }
     
