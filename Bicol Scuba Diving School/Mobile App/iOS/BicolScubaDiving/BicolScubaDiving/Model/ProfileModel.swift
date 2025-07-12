adf817b0374d4733e8de74e396479c45c6675f26
Lenard <lenardcortuna05@gmail.com>
2025-07-12 23:59:21 +0800

    Created ProfileModel.

---
+++ Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/Model/ProfileModel.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileModel.swift
+//  BicolScubaDiving
+//
+//  Created by Lenard Cortuna on 7/9/25.
+//
+
+import UIKit
+
+struct ProfileSection {
+    let sectionTitle: String
+    let items: [ProfileItem]
+}
+
+struct ProfileItem {
+    let title: String
+}
