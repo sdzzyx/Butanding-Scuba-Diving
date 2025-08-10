86077b02e12b3f1c3d1823f81d7ddb0989f57c55
Lenard <lenardcortuna05@gmail.com>
2025-08-10 10:35:50 +0800

    Added DivePackage Model.

---
+++ Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/Models/PackageModel.swift
@@ -0,0 +1,19 @@
+//
+//  PackageModel.swift
+//  BicolScubaDiving
+//
+//  Created by Lenard Cortuna on 8/6/25.
+//
+
+import Foundation
+
+struct DivePackage {
+    let id: String
+    let title: String
+    let description: String
+    let imageUrl: String
+    let price: String
+    let isActive: Bool
+    let totalSlot: Int
+    let bookedSlot: Int
+}
