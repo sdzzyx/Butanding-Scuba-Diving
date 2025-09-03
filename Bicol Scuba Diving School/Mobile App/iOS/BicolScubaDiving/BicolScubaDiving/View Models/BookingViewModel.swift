4d225af48ecaa5a46f9a6d8250f818af47922da1
Lenard <lenardcortuna05@gmail.com>
2025-09-03 10:34:52 +0800

    Added BookingViewMode.

---
+++ Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/View Models/BookingViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  BookingViewModel.swift
+//  BicolScubaDiving
+//
+//  Created by Lenard Cortuna on 9/3/25.
+//
+
+import Foundation
+
+class BookingViewModel {
+    
+    var bookings: [String] = [] // placeholder for now
+    
+    var isLoading: Bool = false
+}
