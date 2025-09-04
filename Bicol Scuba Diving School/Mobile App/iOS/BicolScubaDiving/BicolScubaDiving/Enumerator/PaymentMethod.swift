85745f41316f3c243bfea04e6ce8871e27821325
Lenard <lenardcortuna05@gmail.com>
2025-09-04 18:58:13 +0800

    Added enum PaymentMethod.

---
+++ Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/Enumerator/PaymentMethod.swift
@@ -0,0 +1,14 @@
+//
+//  PaymentMethod.swift
+//  BicolScubaDiving
+//
+//  Created by Lenard Cortuna on 9/4/25.
+//
+
+enum PaymentMethod: CaseIterable {
+    
+    case cash
+    case gcash
+    case paypal
+    
+}
