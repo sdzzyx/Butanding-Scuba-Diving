e450b40f63586e8eaea985deeb5e047f8f1eca36
Lenard <lenardcortuna05@gmail.com>
2025-09-08 17:49:08 +0800

    Added PaymentMethodCell.

---
+++ Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/Views/Payment/View/PaymentMethodCell.swift
@@ -0,0 +1,32 @@
+//
+//  PaymentMethodCell.swift
+//  BicolScubaDiving
+//
+//  Created by Lenard Cortuna on 9/8/25.
+//
+
+import UIKit
+
+class PaymentMethodCell: UITableViewCell {
+    static let identifier = "PaymentMethodCell"
+    
+    let methodRow: PaymentMethodRow
+    
+    init(method: PaymentMethod) {
+        self.methodRow = PaymentMethodRow(iconName: method.iconName,
+                                          title: method.title,
+                                          iconSize: method.iconSize)
+        super.init(style: .default, reuseIdentifier: PaymentMethodCell.identifier)
+        contentView.addSubview(methodRow)
+        methodRow.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(12)
+        }
+        backgroundColor = .clear
+        selectionStyle = .none
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
