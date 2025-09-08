162abd003c07f1347f79c0f91dc42d774bbcf076
Lenard <lenardcortuna05@gmail.com>
2025-09-08 17:47:04 +0800

    Configured PaymentViewModel.

--- Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/View Models/PaymentViewModel.swift
+++ Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/View Models/PaymentViewModel.swift
@@ -7,10 +7,23 @@
 
 import Foundation
 
+enum PaymentMethodSection: Int, CaseIterable {
+    case cash
+    case moreOptions
+    
+    var title: String {
+        switch self {
+        case .cash: return AppConstant.Payment.cashTitle
+        case .moreOptions: return AppConstant.Payment.morePaymentOptionsTitle
+        }
+    }
+}
+
+
 class PaymentViewModel {
     
     // MARK: - Properties
-    private(set) var paymentMethods: [PaymentMethod] = PaymentMethod.allCases
+    private(set) var paymentMethodsBySection: [PaymentMethodSection: [PaymentMethod]] = [:]
     private(set) var selectedMethod: PaymentMethod?
     
     var isLoading: Bool = false {
@@ -29,8 +42,33 @@ class PaymentViewModel {
         isLoading = true
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
             self.isLoading = false
+            
+            // Group methods by section
+            self.paymentMethodsBySection = [
+                .cash: [
+                    PaymentMethod(type: .cash,
+                                  iconName: AppConstant.Payment.cashLogo,
+                                  title: AppConstant.Payment.cashPaymentTitle,
+                                  iconSize: 30)
+                ],
+                .moreOptions: [
+                    PaymentMethod(type: .gcash,
+                                  iconName: AppConstant.Payment.gcashLogo,
+                                  title: nil,
+                                  iconSize: 100),
+                    PaymentMethod(type: .paypal,
+                                  iconName: AppConstant.Payment.paypalLogo,
+                                  title: nil,
+                                  iconSize: 100)
+                ]
+            ]
+            
             self.onPaymentMethodsLoaded?()
         }
+    }
+    
+    func methods(for section: PaymentMethodSection) -> [PaymentMethod] {
+        return paymentMethodsBySection[section] ?? []
     }
     
     func selectMethod(_ method: PaymentMethod) {
