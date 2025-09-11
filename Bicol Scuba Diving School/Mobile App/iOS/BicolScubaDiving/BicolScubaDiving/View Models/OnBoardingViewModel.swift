ac7925ba370b03c68394af0755331cc86c016b91
Melvin Ballesteros <>
2025-09-11 11:01:15 +0800

    Corrected the formatting

--- Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/View Models/OnBoardingViewModel.swift
+++ Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/View Models/OnBoardingViewModel.swift
@@ -25,10 +25,25 @@ class OnboardingViewModel {
     private func setupSlides() {
         
         slides = [
-            WelcomeModel(image: UIImage(named: "Welcome_Screen_1") ?? UIImage(), title: "Bicol Scuba Diving School", subtitle: "Explore the Depths", description: "Unleash your inner explorer as you embark on an exciting, fun program designed to boost your diving skills and deepen your understanding of core diving areas."),
-            WelcomeModel(image: UIImage(named: "Welcome_Screen_2") ?? UIImage(), title: "Discovery Dive",subtitle: "Dive with Confidence", description: "Curious about scuba diving but not ready for a full course? Discovery Dive is the perfect first step into the ocean. You'll learn basic skills and get to experience a real dive under instructor supervision."),
-            WelcomeModel(image: UIImage(named: "Welcome_Screen_3") ?? UIImage(), title: "Professional Dive Course",subtitle: "Get Scuba Certified", description: "Advance your skills and train to become a certified dive professional. This course covers advanced techniques, leadership, and real-world dive scenarios."),
-            WelcomeModel(image: UIImage(named: "Welcome_Screen_4") ?? UIImage(), title: "Dive Training",subtitle: "Learn to Dive with Expert Guidance", description: "Sharpen your dive skills with focused, hands-on training. Learn safety procedures, underwater navigation, and gear handling in guided sessions. Ideal for beginners or divers looking to refresh and improve.")
+            WelcomeModel(image: UIImage(named: "Welcome_Screen_1") ?? UIImage(), 
+                         title: "Bicol Scuba Diving School", 
+                         subtitle: "Explore the Depths", 
+                         description: "Unleash your inner explorer as you embark on an exciting, fun program designed to boost your diving skills and deepen your understanding of core diving areas."),
+            
+            WelcomeModel(image: UIImage(named: "Welcome_Screen_2") ?? UIImage(), 
+                         title: "Discovery Dive",
+                         subtitle: "Dive with Confidence", 
+                         description: "Curious about scuba diving but not ready for a full course? Discovery Dive is the perfect first step into the ocean. You'll learn basic skills and get to experience a real dive under instructor supervision."),
+            
+            WelcomeModel(image: UIImage(named: "Welcome_Screen_3") ?? UIImage(), 
+                         title: "Professional Dive Course",
+                         subtitle: "Get Scuba Certified", 
+                         description: "Advance your skills and train to become a certified dive professional. This course covers advanced techniques, leadership, and real-world dive scenarios."),
+            
+            WelcomeModel(image: UIImage(named: "Welcome_Screen_4") ?? UIImage(), 
+                         title: "Dive Training",
+                         subtitle: "Learn to Dive with Expert Guidance", 
+                         description: "Sharpen your dive skills with focused, hands-on training. Learn safety procedures, underwater navigation, and gear handling in guided sessions. Ideal for beginners or divers looking to refresh and improve.")
         ]
     }
     
