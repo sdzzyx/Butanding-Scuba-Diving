8c6e01a820402b109066e5991f8086ca32627411
Lenard <lenardcortuna05@gmail.com>
2025-09-17 22:45:31 +0800

    Added CompanionModel.

---
+++ Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/Models/CompanionModel.swift
@@ -0,0 +1,12 @@
+//
+//  CompanionModel.swift
+//  BicolScubaDiving
+//
+//  Created by Lenard Cortuna on 9/16/25.
+//
+import UIKit
+
+struct Companion {
+    var fullName: String = ""
+    var medicalCertificate: UIImage? = nil
+}
