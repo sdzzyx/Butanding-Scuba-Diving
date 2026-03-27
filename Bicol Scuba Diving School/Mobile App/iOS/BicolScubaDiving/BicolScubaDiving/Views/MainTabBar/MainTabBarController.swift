c3cbefc663047086cdc6cbd1a8f84071b2c02fea
Lenard <lenardcortuna05@gmail.com>
2026-03-27 18:59:31 +0800

    Updated MainTabController.

--- Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/Views/MainTabBar/MainTabBarController.swift
+++ Bicol Scuba Diving School/Mobile App/iOS/BicolScubaDiving/BicolScubaDiving/Views/MainTabBar/MainTabBarController.swift
@@ -9,7 +9,19 @@ import UIKit
 
 class MainTabBarController: UITabBarController {
     
-    let viewModel = TabBarViewModel()
+    //let viewModel = TabBarViewModel()
+    
+    private let viewModel: TabBarViewModel
+    
+    init(mode: TabBarMode) {
+        self.viewModel = TabBarViewModel(mode: mode)
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+            self.viewModel = TabBarViewModel(mode: .user)
+            super.init(coder: coder)
+        }
 
     override func viewDidLoad() {
         super.viewDidLoad()
